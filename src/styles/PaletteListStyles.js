12be0a4d07d5ad41eb42efc57d515f8e85807376
benjaminsinaiko <ben.jamin@outlook.com>
2019-06-06 11:22:02 -0500

    Add background svg to PaletteList

--- src/styles/PaletteListStyles.js
+++ src/styles/PaletteListStyles.js
@@ -1,10 +1,19 @@
+import sizes from './sizes';
+import bg from './bg.svg';
+
 export default {
   root: {
-    backgroundColor: 'blue',
     height: '100vh',
     display: 'flex',
     alignItems: 'flex-start',
     justifyContent: 'center',
+    backgroundColor: '#5f34af',
+    backgroundImage: `url(${bg})`,
+    /* background by SVGBackgrounds.com */
+    overflow: 'scroll',
+  },
+  heading: {
+    fontSize: '2rem',
   },
   container: {
     width: '50%',
@@ -12,6 +21,12 @@ export default {
     alignItems: 'flex-start',
     flexDirection: 'column',
     flexWrap: 'wrap',
+    [sizes.down('xl')]: {
+      width: '80%',
+    },
+    [sizes.down('xs')]: {
+      width: '70%',
+    },
   },
   nav: {
     display: 'flex',
@@ -28,6 +43,13 @@ export default {
     width: '100%',
     display: 'grid',
     gridTemplateColumns: 'repeat(3, 30%)',
-    gridGap: '5%',
+    gridGap: '2.5rem',
+    [sizes.down('md')]: {
+      gridTemplateColumns: 'repeat(2, 50%)',
+    },
+    [sizes.down('xs')]: {
+      gridTemplateColumns: 'repeat(1, 100%)',
+      gridGap: '1.4rem',
+    },
   },
 };

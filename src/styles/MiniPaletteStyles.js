1fc089dc57d1db767d89b37f5301ca5494e99cbc
benjaminsinaiko <ben.jamin@outlook.com>
2020-02-05 17:21:28 -0600

    Update background and other styles

--- src/styles/MiniPaletteStyles.js
+++ src/styles/MiniPaletteStyles.js
@@ -1,6 +1,6 @@
 export default {
   root: {
-    backgroundColor: 'white',
+    backgroundColor: '#303542',
     border: '1px solid black',
     borderRadius: '5px',
     padding: '0.5rem',
@@ -8,29 +8,29 @@ export default {
     overflow: 'hidden',
     cursor: 'pointer',
     '&:hover svg': {
-      opacity: 1,
-    },
+      opacity: 1
+    }
   },
   colors: {
-    backgroundColor: '#dae1e4',
+    backgroundColor: '#303542',
     height: '150px',
     width: '100%',
     borderRadius: '5px',
-    overflow: 'hidden',
+    overflow: 'hidden'
   },
   title: {
     display: 'flex',
     justifyContent: 'space-between',
     alignItems: 'center',
     margin: '0',
-    color: 'black',
+    color: '#cecfd3',
     paddingTop: '0.5rem',
     fontSize: '1rem',
-    position: 'relative',
+    position: 'relative'
   },
   emoji: {
     marginLeft: '0.5rem',
-    fontSize: '1.5rem',
+    fontSize: '1.5rem'
   },
   miniColor: {
     height: '25%',
@@ -38,7 +38,7 @@ export default {
     display: 'inline-block',
     margin: '0 auto',
     position: 'relative',
-    marginBottom: '-3.5px',
+    marginBottom: '-3.5px'
   },
   deleteIcon: {
     color: 'white',
@@ -50,6 +50,6 @@ export default {
     top: '0px',
     padding: '10px',
     zIndex: 10,
-    opacity: 0,
-  },
+    opacity: 0
+  }
 };
--- src/styles/PaletteListStyles.js
+++ src/styles/PaletteListStyles.js
@@ -1,28 +1,32 @@
 import sizes from './sizes';
-import bg from './bg.svg';
+import bg from './bg.svg'
 
 export default {
   '@global': {
     '.fade-exit': {
-      opacity: 1,
+      opacity: 1
     },
     '.fade-exit-active': {
       opacity: 0,
-      transition: 'opacity 500ms ease-out',
-    },
+      transition: 'opacity 500ms ease-out'
+    }
   },
   root: {
     height: '100vh',
     display: 'flex',
     alignItems: 'flex-start',
     justifyContent: 'center',
-    backgroundColor: '#5f34af',
-    backgroundImage: `url(${bg})`,
-    /* background by SVGBackgrounds.com */
-    overflow: 'scroll',
+    backgroundColor: '#f0f0f0',
+    backgroundImage: `url(${bg})`, // background by SVGBackgrounds.com
+    backgroundAttachment: 'fixed',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'top left',
+    overflow: 'scroll'
   },
   heading: {
     fontSize: '2rem',
+    color: '#303542',
+    fontWeight: 'bold',
   },
   container: {
     width: '50%',
@@ -31,21 +35,37 @@ export default {
     flexDirection: 'column',
     flexWrap: 'wrap',
     [sizes.down('xl')]: {
-      width: '80%',
+      width: '80%'
     },
     [sizes.down('xs')]: {
-      width: '70%',
-    },
+      width: '70%'
+    }
   },
   nav: {
+    position: 'relative',
     display: 'flex',
     width: '100%',
-    justifyContent: 'space-between',
+    justifyContent: 'center',
     alignItems: 'center',
     color: 'white',
     '& a': {
-      color: 'white',
+      position: 'absolute',
+      right: 0,
+      color: '#f2bd2c',
     },
+    [sizes.down('xs')]: {
+      position: 'inherit',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      '& h1': {
+        color: '#fff',
+        marginBottom: '16px'
+      },
+      '& a': {
+        position: 'inherit',
+        marginBottom: '16px'
+      }
+    }
   },
   palettes: {
     boxSizing: 'border-box',
@@ -54,11 +74,11 @@ export default {
     gridTemplateColumns: 'repeat(3, 30%)',
     gridGap: '2.5rem',
     [sizes.down('md')]: {
-      gridTemplateColumns: 'repeat(2, 50%)',
+      gridTemplateColumns: 'repeat(2, 50%)'
     },
     [sizes.down('xs')]: {
       gridTemplateColumns: 'repeat(1, 100%)',
-      gridGap: '1.4rem',
-    },
-  },
+      gridGap: '1.4rem'
+    }
+  }
 };
